03182e9dcefd21ce2479352c415eb9df67b65040
NITHYSK <elochai000@gmail.com>
2021-04-24 13:41:46 +0900

    "ファイルを分割した"

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 git init
 git add 9cc.h Makefile codegen.c dbg.h error.c global.c main.c parse.c test.sh test1.sh tokenize.c update.sh .gitignore
 git commit -m "\"$1\""
-git log -p
+#git log -p
 git remote add origin git@github.com:NITHYSK/9cc.git
 git push
 
