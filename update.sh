fe35325aed094c7e5b68cc00f0f8679b925b6c03
NITHYSK <elochai000@gmail.com>
2021-06-07 08:31:03 +0900

    変数の扱いを変更

--- update.sh
+++ update.sh
@@ -5,5 +5,5 @@ git add README Makefile src include *.sh .gitignore lib obj
 git commit -m "\"$1\""
 #git log -p
 git remote add origin git@github.com:NITHYSK/9cc.git
-git push
+git push -f origin master
 
